20503db4c1cb8748fda64505741e426836fab7af
timohy <timothy820712@gmail.com>
2015-01-11 15:30:36 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,8 +1,16 @@
 
 $(function() {
-	$('#slides').slidesjs({
+	$('#slide').slidesjs({
+		width: 1000,
+		height: 360,
+		navigation: false
+	});
+
+	$('#slidereset').slidesjs({
 		width: 1000,
 		height: 360,
 		navigation: false
 	});
 });
+
+
